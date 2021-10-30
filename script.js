81b41ea45c2d738e14b69344212aafd646633384
sahil <sahil@gmail.com>
2021-10-30 10:11:44 +0530

    posts in search section

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
-    $('#menu').click(function(){
-        $(this).toggleClass('fa-times');
-        $('.nav').toggleClass('nav-toggle')
+    $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-times')
+        $('nav').toggleClass('nav-toggle')
     })
-    $(window).on('scroll load', function(){
-        $('.fa-bars').removeClass('fa-times')
-        $('.nav').removeClass('nav-toggle')
-    })
+})
+
+$(window).on('scroll load', function(){
+    $('.fa-bars').removeClass('fa-times')
+    $('nav').removeClass('nav-toggle')
 })
